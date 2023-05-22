56a1a29d66aa4be22a2ff1df3f70a7b32faa044c
Philipp Czernitzki <i@pczern.dev>
2023-05-22 23:26:43 +0200

    update action

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const KEY_TITLE_REGEX = "titleRegex";
 const KEY_DESCRIPTION_REGEX = "descriptionRegex";
 const KEY_TITLE_MIN_LENGTH = "titleMinLength";
 const KEY_DESCRIPTION_MIN_LENGTH = "descriptionMinLength";
+const GET_LABELS_QUERY = `query GetRepositoryLabelsQuery($repositoryId: ID!){
+	node(id: $repositoryId){
+    ... on Repository {
+      labels {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+}`;
 const UPDATE_PULL_REQUEST_MUTATION = `mutation UpdatePullRequestMutation($pullRequestId:ID!, $labelIds:[ID!]) {
   updatePullRequest(input:{pullRequestId:$pullRequestId, labelIds:$labelIds}){pullRequest {
     id
@@ -38,7 +52,8 @@ async function action() {
     core.setOutput("time", time);
     const payload = github.context.payload;
 
-    const pullRequestId = payload.pull_request.id;
+    const pullRequestId = payload.pull_request.node_id;
+    const repositoryId = payload.repository.node_id;
     const pullRequestRepositoryUrl = payload.repository.url;
     const pullRequestRepositoryName = payload.repository.name;
     const pullRequestRepositoryOwnerLogin = payload.repository.owner.login;
@@ -63,39 +78,48 @@ async function action() {
         );
         console.log(response);
 
-        return response?.node_id;
+        return name;
       } catch {
-        return null;
+        return name;
       }
     };
 
     const errors = [];
-    const newLabelIds = [];
+    const newLabels = [];
 
     if (!titleRegex.exec(payload.pull_request.title)) {
       errors.push("Title doesn't match Regex!");
-      newLabelIds.push(await createLabel(LABEL_TITLE_FORMAT));
+      newLabels.push(await createLabel(LABEL_TITLE_FORMAT));
     }
     if (!descriptionRegex.exec(payload.pull_request.body)) {
       errors.push("Description doesn't match Regex!");
-      newLabelIds.push(await createLabel(LABEL_DESCRIPTION_FORMAT));
+      newLabels.push(await createLabel(LABEL_DESCRIPTION_FORMAT));
     }
     if (payload.pull_request.title.length < titleMinLength) {
       errors.push("Title isn't long enough!");
-      newLabelIds.push(await createLabel(LABEL_TITLE_LENGTH));
+      newLabels.push(await createLabel(LABEL_TITLE_LENGTH));
     }
     if (payload.pull_request?.description?.length < descriptionMinLength) {
       errors.push("Description isn't long enough!");
-      newLabelIds.push(await createLabel(LABEL_DESCRIPTION_LENGTH));
+      newLabels.push(await createLabel(LABEL_DESCRIPTION_LENGTH));
     }
     console.log("labelids", labelIds);
     console.log("newLabelIds", newLabelIds);
+
+    const { data } = await octokit.graphql(GET_LABELS_QUERY, {
+      repositoryId,
+    });
+    const assignableLabels = newLabels.filter((label) => !!label);
+    const errorLabels = data.node.labels.edges
+      .map(({ node: label }) => label)
+      .filter(assignableLabels.includes(label.name))
+      .map((label) => label.id);
 
     if (errors.length > 0) {
       console.log(labelIds, pullRequestId);
       await octokit.graphql(UPDATE_PULL_REQUEST_MUTATION, {
         pullRequestId,
-        labelIds: labelIds.concat(newLabelIds.filter((labelId) => !!labelId)),
+        labelIds: labelIds.concat(errorLabels),
       });
 
       throw new Error(errors.join("\n"));
