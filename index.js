276efaa4f1b8d290427716d20041664fa3daff9c
Philipp Czernitzki <i@pczern.dev>
2023-05-22 21:25:21 +0200

    Update code

--- index.js
+++ index.js
@@ -23,63 +23,76 @@ const allLabels = [
   LABEL_TITLE_LENGTH,
   LABEL_DESCRIPTION_LENGTH,
 ];
+async function action() {
+  try {
+    const titleRegex = new RegExp(core.getInput(KEY_TITLE_REGEX));
+    const descriptionRegex = new RegExp(core.getInput(KEY_DESCRIPTION_REGEX));
+    const titleMinLength = core.getInput(KEY_TITLE_MIN_LENGTH);
+    const descriptionMinLength = core.getInput(KEY_DESCRIPTION_MIN_LENGTH);
 
-const createLabel = async (name) => {
-  // const response = await octokit.request("POST /repos/{owner}/{repo}/labels", {
-  //   owner: "OWNER",
-  //   repo: "REPO",
-  //   name,
-  //   color: "ff0000",
-  //   headers: {
-  //     "X-GitHub-Api-Version": "2022-11-28",
-  //   },
-  // });
-  // return response?.node_id;
-};
-try {
-  const titleRegex = new RegExp(core.getInput(KEY_TITLE_REGEX));
-  const descriptionRegex = new RegExp(core.getInput(KEY_DESCRIPTION_REGEX));
-  const titleMinLength = core.getInput(KEY_TITLE_MIN_LENGTH);
-  const descriptionMinLength = core.getInput(KEY_DESCRIPTION_MIN_LENGTH);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
 
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+    const pullRequestId = payload.pull_request.id;
+    const pullRequestRepositoryUrl = payload.pull_request.repository.url;
+    const pullRequestRepositoryName = payload.pull_request.repository.name;
+    const pullRequestRepositoryOwnerLogin =
+      payload.pull_request.repository.owner.login;
+    const labelIds = payload.pull_request.labels
+      .filter((label) => !allLabels.includes(label.name))
+      .map((label) => label.id);
 
-  const pullRequestId = payload.pull_request.id;
-  const labelIds = payload.pull_request.labels
-    .filter((label) => !allLabels.includes(label.name))
-    .map((label) => label.id);
+    const createLabel = async (name) => {
+      const response = await octokit.request(
+        `POST ${pullRequestRepositoryUrl}/labels`,
+        {
+          owner: pullRequestRepositoryOwnerLogin,
+          repo: pullRequestRepositoryName,
+          name,
+          color: "ff0000",
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+      console.log(response);
+      return response?.node_id;
+    };
 
-  const errors = [];
-  const newLabelIds = [];
+    const errors = [];
+    const newLabelIds = [];
 
-  if (!titleRegex.exec(payload.pull_request.title)) {
-    errors.push("Title doesn't match Regex!");
-    labels.push(createLabel(LABEL_TITLE_FORMAT));
+    if (!titleRegex.exec(payload.pull_request.title)) {
+      errors.push("Title doesn't match Regex!");
+      labels.push(await createLabel(LABEL_TITLE_FORMAT));
+    }
+    if (!descriptionRegex.exec(payload.pull_request.body)) {
+      errors.push("Description doesn't match Regex!");
+      labels.push(await createLabel(LABEL_DESCRIPTION_FORMAT));
+    }
+    if (payload.pull_request.title.length < titleMinLength) {
+      errors.push("Title isn't long enough!");
+      labels.push(await createLabel(LABEL_TITLE_LENGTH));
+    }
+    if (payload.pull_request?.description?.length < descriptionMinLength) {
+      errors.push("Description isn't long enough!");
+      labels.push(await createLabel(LABEL_DESCRIPTION_LENGTH));
+    }
+
+    if (errors.length > 0) {
+      console.log(labelIds, pullRequestId);
+      await octokit.graphql(GRAPHQL_QUERY, {
+        pullRequestId,
+        labelIds: labelIds.concat(newLabelIds),
+      });
+
+      throw new Error(errors.join("\n"));
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  if (!descriptionRegex.exec(payload.pull_request.body)) {
-    errors.push("Description doesn't match Regex!");
-    labels.push(createLabel(LABEL_DESCRIPTION_FORMAT));
-  }
-  if (payload.pull_request.title.length < titleMinLength) {
-    errors.push("Title isn't long enough!");
-    labels.push(createLabel(LABEL_TITLE_LENGTH));
-  }
-  if (payload.pull_request?.description?.length < descriptionMinLength) {
-    errors.push("Description isn't long enough!");
-    labels.push(createLabel(LABEL_DESCRIPTION_LENGTH));
-  }
-
-  if (errors.length > 0) {
-    octokit.graphql(GRAPHQL_QUERY, {
-      pullRequestId,
-      labelIds: labelIds.concat(newLabelIds),
-    });
-
-    throw new Error(errors.join("\n"));
-  }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+action();
